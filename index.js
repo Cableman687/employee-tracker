7e3539a91ebacad976a35878d22ba6516db2642d
William <will_cable@hotmail.com>
2023-02-14 13:02:14 +0800

    Addition of tables to schema.sql to contain department, employee, and role-related data

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-const express = require('express');
-const path = require('path');
-
-// Import and require mysql2
-// const mysql = require('mysql2');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-
-// Express middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 //Include Prompts
 const prompts = require("./src/prompts");
+
+
+// Import and require mysql2 & express
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL Username
+      user: 'root',
+      // TODO: Add MySQL Password
+      password: 'password123',
+      database: 'employeeTracker_db'
+    },
+    console.log(`Connected to the employeeTracker_db database.`)
+  );
+
+
+console.log("Server file triggered");
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 
 //--------------------Master Prompts / Request User Direction------------------------
 
 
 
 async function userRequest(){
+
+   
 
     welcomeResponse = await prompts.welcomePrompts();
 
@@ -32,6 +52,3 @@ async function userRequest(){
 
 userRequest();
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
