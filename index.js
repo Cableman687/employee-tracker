27b5995279d1d7d2be13396623770475137c4b6f
William <will_cable@hotmail.com>
2023-02-17 20:07:41 +0800

    Major reformatting of files in response to modularity errors and communication between files

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //Include Prompts
 const prompts = require("./src/prompts");
 const queries = require("./src/queries");
-const builders = require("./src/builders");
 
 // Import and require mysql2, express & console.table
 const express = require('express');
@@ -32,6 +31,12 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
+let roleTable;
+let roleIndex;
+let employeeTable;
+let employeeIndex;
+let updateTable;
+let roleArray = [];
 
 //--------------------Master Switch & Prompt Navigation------------------------
 
@@ -46,46 +51,46 @@ async function masterSwitch(){
 
     case "View All Departments":
 
-      await builders.viewDepartments();
+      await viewDepartments();
       
       break;
 
     case "View All Roles":
 
-      await builders.viewRoles();
+      await viewRoles();
 
       break;
     
     case "View All Employees":
 
 
-      await builders.viewEmployees();
+      await viewEmployees();
 
       break;
     
     case "Add Department":
 
 
-      await builders.addDepartment();
+      await addDepartment();
 
       break;
     
     case "Add Role":
 
-      await builders.getDepartments();
+      await getDepartments();
       
     
     break;
     
     case "Add Employee":
 
-      await builders.getManagers();
+      await getManagers();
 
     break;
 
     case "Update Employee Role":
 
-      await builders.getRoles();
+      await getRoles();
 
     break;
     
@@ -98,16 +103,228 @@ async function masterSwitch(){
 
 }
 
+//---------------------------------------View Departments----------------------------------------
+async function viewDepartments(){
+  db.query(await queries.viewDepartments(), function (err, results) {
+  
+    console.table(results);
+    init();
+  })
+}
+//---------------------------------------View Roles----------------------------------------------
+
+async function viewRoles(){
+
+  db.query(await queries.viewRoles(), function (err, results) {
+
+    console.table(results);
+    init();
+    })
+
+}
+
+//---------------------------------------View Employees------------------------------------------
+
+async function viewEmployees(){
+  db.query(await queries.viewEmployees() , function (err, results) {
+    
+    console.table(results);
+    init();
+    })
+}
+
+//---------------------------------------Add Department----------------------------------------
+
+async function addDepartment(){
+
+  db.query(await queries.addDepartment() , function (err, results) {
+
+    console.log("Department Added");
+    init();
+    })
+
+}
+
+
+//---------------------------------------Add Role Function----------------------------------------
+async function getDepartments(){
+
+    let departmentArray = [];
+
+    db.query('SELECT department_name FROM departments', async function (err, results) {
+
+      for(const element of results){
+
+        departmentArray.push(element.department_name);
+
+      }
+
+      let role = await prompts.addRolePrompts(departmentArray);
+
+      
+
+      db.query('SELECT * FROM departments', async function (err, results) {
+      roleTable = results;
+      
+
+      for(const element of roleTable){
+        if(element.department_name == role.roleDepartment){
+          
+          roleIndex = element.id;
+          
+        }
+      }
+
+      db.query(await queries.addRole(role, roleIndex) , function (err, results) {
+
+      console.log("Role Added");
+      init();
+    })
+  })
+
+    })
+  }
+
+  //---------------------------------------Add Employee Function----------------------------------------
+
+  async function getManagers(){
+    let getRoleArray = [];
+    let managerIndex;
+
+    db.query('SELECT job_title FROM roles', async function(err,results){
+      for(const element of results){
+        getRoleArray.push(element.job_title);
+      }
+
+      async function getNames(){
+        
+        let nameArray = [];
+
+        db.query('SELECT first_name, last_name, id FROM employees', async function (err, results){
+          for(const element of results){
+            let fullName = element.first_name + " " + element.last_name;
+            nameArray.push(fullName);
+          }
+
+          await prompts.addEmployeePrompts(getRoleArray, nameArray).then((response) => {
+            employee = response;
+            
+
+            
+            db.query('SELECT CONCAT(first_name," ", last_name) AS name, id FROM employees', async function(err, result){
+              for(const element of result){
+                if(employee.employeeManager == element.name){
+                  managerIndex = element.id;
+                }
+
+              }
+
+              db.query('SELECT * FROM roles', async function (err, results) {
+                employeeTable = results;
+                
+          
+                for(const element of employeeTable){
+                  if(element.job_title == employee.employeeRole){
+                    
+                    employeeIndex = element.id;
+                    
+                  }
+                }
+          
+                db.query(await queries.addEmployee(employee, employeeIndex, managerIndex) , function (err, results) {
+                  console.log("Employee Added");
+                  init();
+                })
+              })
+            })
+          })
+        })
+      }
+
+      getNames();
+    })
+  }
+
+ //---------------------------------------Update Employee Role----------------------------------------
+
+  async function getRoles(){
+
+    let nameArray = [];
+
+    db.query('SELECT job_title FROM roles',  async function (err,results){
+      for(const element of results){
+        
+        roleArray.push(element.job_title);
+
+      }
+
+      async function getNames(){
+
+        let result;
+        
+        db.query('SELECT first_name, last_name FROM employees', async function (err,results){
+  
+          
+  
+          for(const element of results){
+  
+              let fullName = element.first_name + " " + element.last_name;
+              nameArray.push(fullName);
+          }
+
+
+          //-----------wait for prompts completion, and store result (name and job id) as array------
+          await prompts.updateEmployeePrompts(nameArray, roleArray).then((response) => {
+  
+          result = response;  
+  
+          const splitName = result.selectEmployee.split(' ');
+          const firstName = splitName[0];
+          const lastName = splitName[1];
+          let updateIndex;
+
+            //---------generate roles table and look for similar first and last names------
+  
+          db.query('SELECT * FROM roles', async function (err, results) {
+            updateTable = results;
+            
+  
+            for(const element of updateTable){
+              if(result.selectRole == element.job_title){
+                updateIndex =  element.id;
+              }
+            }
+  
+            db.query(await queries.updateEmployeeRole(firstName, lastName, updateIndex) , function (err, results) {
+
+              console.log("Employee Updated!");
+              init();
+            })
+
+          })
+  
+          });
+        })
+  
+      }
+
+      getNames();
+      
+    })
+
+  }
+
+
+
+
  //--------------------Initiate Application------------------------
-export async function init(){
+async function init(){
 
   await masterSwitch();
 
 }
 
 init();
-
-module.exports = {init};
 
 
 
